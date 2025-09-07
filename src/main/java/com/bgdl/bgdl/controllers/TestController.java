0e88aaa951c7da6a80aae80cb1ceaa1eed64ae93
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2025-09-07 22:30:30 +0300

    Update TestController.java

--- src/main/java/com/bgdl/bgdl/controllers/TestController.java
+++ src/main/java/com/bgdl/bgdl/controllers/TestController.java
@@ -1,9 +1,5 @@
 package com.bgdl.bgdl.controllers;
 
-import com.bgdl.bgdl.deserializers.LevelSearchResponseDeserializer;
-import com.bgdl.bgdl.exceptions.common.BadRequestException;
-import com.bgdl.bgdl.models.dto.gd.GDLevelDTO;
-import com.bgdl.bgdl.services.GdRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/test")
 public class TestController {
-    private final GdRequestService gdRequestService;
-    private final LevelSearchResponseDeserializer levelSearchResponseDeserializer;
-
-    @GetMapping("/levels/{levelId}")
-    public GDLevelDTO getLevels(@PathVariable String levelId) {
-        String levelData = gdRequestService.getLevelById(levelId);
-
-        if (levelData.equals("-1")) {
-            throw new BadRequestException("Invalid level ID");
-        }
-
-        return levelSearchResponseDeserializer.apply(levelData).get(0);
+    @GetMapping("/{id}")
+    public String getTest(@PathVariable String id) {
+        return "For dev test purposes";
     }
 }

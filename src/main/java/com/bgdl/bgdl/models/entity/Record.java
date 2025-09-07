c61b66d14e253586f17c7892c3d9e93e9fcb8724
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2025-09-07 20:35:32 +0300

    Add records entity

--- src/main/java/com/bgdl/bgdl/models/entity/Record.java
+++ src/main/java/com/bgdl/bgdl/models/entity/Record.java
@@ -1,8 +1,12 @@
 package com.bgdl.bgdl.models.entity;
 
+import com.bgdl.bgdl.enums.RecordStatus;
 import com.bgdl.bgdl.models.baseEntity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -14,5 +18,34 @@ import lombok.*;
 @Entity
 @Table(name = "records")
 public class Record extends BaseEntity {
+    @NotNull
+    @Min(value = 0, message = "Progress must be between 0-100")
+    @Max(value = 100, message = "Progress must be between 0-100")
+    private int progress;
+
+    @NotNull
+    @NotBlank(message = "Youtube URL is required")
+    private String youtubeUrl;
+
+    @NotNull
+    @NotBlank(message = "Raw footage URL is required")
+    private String rawFootageUrl;
+
+    @NotNull
+    @NotBlank(message = "Description is required")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "demon_id")
+    @NotNull(message = "Demon is required")
+    private Demon demon;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RecordStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @NotNull(message = "Demon is required")
+    private User holder;
 }
---
+++ src/main/resources/db/migration/V7__Create_records_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE records
+(
+    id              UUID PRIMARY KEY,
+    progress        INT          NOT NULL CHECK (progress >= 0 AND progress <= 100),
+    youtube_url     VARCHAR(255) NOT NULL CHECK (youtube_url <> ''),
+    raw_footage_url VARCHAR(255) NOT NULL CHECK (raw_footage_url <> ''),
+    description     TEXT         NOT NULL CHECK (description <> ''),
+    status          VARCHAR(50)  NOT NULL CHECK (((status)::text = ANY ((ARRAY['ACCEPTED':: character varying, 'PENDING':: character varying, 'REJECTED':: character varying])::text[]))),
+    demon_id        UUID         NOT NULL,
+    user_id         UUID         NOT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_records_demon FOREIGN KEY (demon_id) REFERENCES demons (id),
+    CONSTRAINT fk_records_user FOREIGN KEY (user_id) REFERENCES users (id)
+);
